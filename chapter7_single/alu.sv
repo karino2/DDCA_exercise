1da826b796ab5d6e53774b5f120a7be516c3a94e
Kazuma Arino <hogeika2ugmail.com>
2019-09-26 22:31:04 +0900

    mips, initial version.

---
+++ chapter7_single/alu.sv
@@ -0,0 +1,122 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/09/23 19:48:54
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module adder(
+    input logic [31:0] a, b,
+    input logic cin,
+    output logic [31:0] s,
+    output logic cout);
+    
+    assign {cout, s} = a+b+cin;
+
+endmodule
+
+module subtractor(input logic [31:0] a, b,
+                   output logic [31:0] y);                
+    assign y = a-b;
+endmodule
+
+module comparator(input logic [31:0] a, b,
+                output logic eq, neq, lt, lte, gt, gte);
+    assign eq = (a == b);
+    assign neq = (a != b);
+    assign lt = (a < b);
+    assign lte = (a <= b);
+    assign gt = (a > b);
+    assign gte = (a >= b);
+endmodule
+
+module mux4(input logic [31:0] d0, d1, d2, d3,
+    input logic [1:0]s,
+    output logic [31:0]y
+    );
+    always_comb
+    case(s)
+        2'd0: y=d0;
+        2'd1: y=d1;
+        2'd2: y=d2;
+        2'd3: y=d3;
+    endcase
+endmodule
+
+module mux2(input logic [31:0] d0, d1,
+    input logic s,
+    output logic [31:0]y
+    );
+    assign y = s ? d1 : d0;
+endmodule
+
+module alu(
+    input logic [31:0] a, b,
+    input logic [2:0] f,
+    output logic cout,
+    output logic zero,
+    output logic [31:0] y
+    );
+    
+    logic [31:0] bb, s;
+
+    mux2 bmux(b, ~b, f[2], bb);
+    adder aluadder(a, bb, f[2], s, cout);
+    mux4 lastmux(a&bb, a|bb, s, {{31{0}}, s[31]}, f[1:0], y);
+    asign zero = (y == 0);
+endmodule
+
+/*
+module testbench_alu();
+    logic [31:0] a, b, y;
+    logic [2:0] f;
+    logic cout;
+    
+    alu dut(a, b, f, cout, y);
+    initial begin
+        // add
+        f = 3'b010;
+        a = 3; b = 5; #10;
+        assert(y === 8 & cout===0) else $error("fail 3+5");
+        a = 32'h8000_0000; b = 32'h8000_0000; #10;
+        assert(y === 0 & cout===1) else $error("fail cout");
+        
+        // subtract
+        f = 3'b110;
+        a = 12; b=24; #10;
+        assert(y === -12 & cout===0) else $error("fail 12-24");
+       
+        // and 
+        f = 3'b000;
+        a = 32'b01011; b = 32'b111; #10;
+        assert(y === 32'b011) else $error("fail and");
+
+        // or
+        f = 3'b001; #10;
+        assert(y === 32'b01111) else $error("fail or, %b", y);
+                
+        // slt
+        f = 3'b111;
+        a = 32; b = 31; #10;
+        assert(y === 0) else $error("fail slt false case. %b", y);
+        a = 31; b = 40; #10;
+        assert(y === 1) else $error("fail slt true case. %b", y);
+        
+    end
+    
+endmodule
+*/
---
+++ chapter7_single/mips.sv
@@ -0,0 +1,122 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2019/09/24 16:45:28
+// Design Name: 
+// Module Name: mips
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module ctrlunit(input logic [5:0] Opcode, input logic [5:0] Funct, 
+    output logic RegWrite, output logic RegDst, output logic ALUSrc, output logic Branch, 
+    output logic MemWrite, output logic MemtoReg, output logic [2:0] ALUCtrl, output logic Jump);
+    
+    assign RegWrite = ((Opcode == 0) | (Opcode == 6'b100011) | (Opcode == 6'b001000));
+    assign RegDst = Opcode == 0;
+    assign ALUSrc = ((Opcode != 0) & (Opcode != 6'b000100));
+    assign Branch = Opcode == 6'b000100;
+    assign MemWrite = Opcode == 6'b101011;
+    assign MemtoReg = Opcode == 6'b100011;
+    assign ALUCtrl = ((Opcode == 6'b000100) | ((Opcode == 0) & (Funct == 6'd34))) ? 3'b110 : 3'b010;
+    assign Jump = Opcode == 6'b000010;     
+endmodule
+
+/*
+module testbench_ctrlunit();
+    logic [5:0] Opcode, Funct;
+    logic RegWrite, RegDst, ALUSrc, Branch; 
+    logic MemWrite, MemtoReg, Jump;
+    logic [2:0] ALUCtrl;
+    
+    ctrlunit dut(Opcode, Funct, RegWrite, RegDst, ALUSrc, Branch, MemWrite, MemtoReg, ALUCtrl, Jump);
+    
+    initial begin
+        // add
+        Opcode = 0; Funct = 6'd32; #10;
+        assert(RegWrite & RegDst & ~ALUSrc & ~Branch & ~MemWrite & ~MemtoReg & (ALUCtrl == 3'b010) & ~Jump) else $error("fail add");
+        // sub
+        Funct = 6'd34; #10;
+        assert(RegWrite & RegDst & ~ALUSrc & ~Branch & ~MemWrite & ~MemtoReg & (ALUCtrl == 3'b110) & ~Jump) else $error("fail sub");
+        
+        // beq
+        Opcode = 6'b000100; Funct = 0; #10;
+        assert(~RegWrite & ~ALUSrc & Branch & ~MemWrite & (ALUCtrl == 3'b110) & ~Jump) else $error("fail beq");
+
+        // j
+        Opcode = 6'b000010; #10;
+        assert(~RegWrite & ~MemWrite & Jump) else $error("fail j");
+        
+        // lw
+        Opcode = 6'b100011; #10;
+        assert(RegWrite & ~RegDst & ALUSrc & ~Branch & ~MemWrite & MemtoReg & (ALUCtrl == 3'b010) & ~Jump) else $error("fail lw");
+        
+                
+        
+        
+    end
+
+endmodule
+*/
+
+module all(
+    input logic clk,
+    input logic reset
+    );
+    logic [31:0] newPC, pc, instr;
+    logic RegWrite, RegDst, ALUSrc, Branch; 
+    logic MemWrite, MemtoReg, Jump;
+    logic [2:0] ALUCtrl;
+    
+    
+    flopr Pcflop(clk, reset, newPC, pc);
+    romcode InstRom(pc[13:0], instr);
+    
+    ctrlunit CtrlUnit(instr[31:26], instr[5:0], RegWrite, RegDst, ALUSrc, Branch, MemWrite, MemtoReg, ALUCtrl, Jump);
+    
+    logic [4:0] a3;
+    logic [31:0] rd1, rd2;
+    logic [31:0] regRes;    
+    
+    assign a3 = RegDst? instr[15:11] : instr[20:16]; 
+    regfile RegFile(clk, instr[25:21], instr[20:16], a3, RegWrite, regRes, rd1, rd2);
+   
+    
+    
+    logic [31:0] signImm, srcB, alures;
+    
+    assign signImm = {{16{instr[15]}}, instr[15:0]};
+    
+    logic cout, zero;
+    
+    mux2 MuxSrcB(rd2, signImm, ALUSrc, srcB); 
+    
+    alu Alu(rd1, srcB, ALUCtrl, cout, zero, alures);
+
+    logic [31:0] readData;    
+    sram DataMem(clk, alures, MemWrite, rd2, readData);
+    
+    mux2 ResForReg(alures, readData, MemtoReg, regRes);
+ 
+    logic [31:0] pcPlus4, pcBranch, pcCand1, pcJump;
+    
+    assign pcPlus4 = pc+4;
+    assign pcBranch = {signImm[29:0], 4'b0000}+pcPlus4;
+    assign pcJump = {pcPlus4[31:28], instr[25:0], 2'b00};
+    
+    
+    assign pcCand1 = (zero & Branch) ? pcBranch : pcPlus4;
+    assign newPC = Jump?pcJump : pcCand1;
+    
+endmodule
