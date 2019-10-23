17a631d1af96b00f19edc5ff9ebb19f5f44480b3
Kazuma Arino <hogeika2@gmail.com>
2019-10-23 11:44:45 +0900

    Use label and confirm result the same.

--- asm/asm_files/simt_beq_backward.s
+++ asm/asm_files/simt_beq_backward.s
@@ -9,7 +9,7 @@ addi $2, $0, 2 // target, core 2.
 
 
 beq $2, $31, 1
-j 11 // goto 0x2c
+j other // goto 0x2c
 
 // 0x0010
 // only exexure core 2. add 1 to 9. result is in $3.
@@ -22,10 +22,12 @@ addi $1, $1, 1
 // 0x0020
 slt $5, $4, $1
 beq $5, $0, -5 // goback if $1 != 10
-j 12 // goto 0x0030
+j common
 
+other:
 addi $3, $0, 1 // 0x002c, jump from above.
-// 0x0030, exec both
+
+common: // exec both
 muli $1, $31, 4
 sw $3, $1, 0
 halt
